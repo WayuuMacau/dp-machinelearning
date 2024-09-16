c25ec5dbc4da67e5cb4a451556729fdd99351e89
WayuuMacau <157907003+WayuuMacau@users.noreply.github.com>
2024-09-16 18:36:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,11 +65,11 @@ def target_encode(val):
 
 y = y_raw.apply(target_encode)
 
-with st.expander('Data preparation'):
-  st.write('**Encoded X (input penguin)**')
-  input_row
-  st.write('**Encoded y**')
-  y
+#with st.expander('Data preparation'):
+#  st.write('**Encoded X (input penguin)**')
+#  input_row
+#  st.write('**Encoded y**')
+#  y
 
 
 

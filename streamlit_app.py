b0775ac01f083890edb48f4e850b05389af72fc9
WayuuMacau <157907003+WayuuMacau@users.noreply.github.com>
2024-09-16 18:23:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,32 +47,6 @@ with st.sidebar:
 with st.expander('Input features'):
   st.write('**Input penguin**')
   input_df
-  st.write('**Combined penguins data**')
-  input_penguins
-
-
-# Data preparation
-# Encode X
-encode = ['island', 'sex']
-df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-
-X = df_penguins[1:]
-input_row = df_penguins[:1]
-
-# Encode y
-target_mapper = {'Adelie': 0,
-                 'Chinstrap': 1,
-                 'Gentoo': 2}
-def target_encode(val):
-  return target_mapper[val]
-
-y = y_raw.apply(target_encode)
-
-with st.expander('Data preparation'):
-  st.write('**Encoded X (input penguin)**')
-  input_row
-  st.write('**Encoded y**')
-  y
 
 
 # Model training and inference

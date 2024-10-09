32e2fa5e11b96c8a5afd7073108f594f008abdab
WayuuMacau <157907003+WayuuMacau@users.noreply.github.com>
2024-10-09 13:00:49 +0800

    Update streamlit_regression.py

--- streamlit_regression.py
+++ streamlit_regression.py
@@ -145,14 +145,14 @@ with st.expander('Correlation & Feature Importances'):
 
 # Random Forest Regressor Metrics
 with st.expander('Random Forest Regressor Metrics'):
-    st.caption('Train set 80%, Test set 20%; Sampling without replacement')
+    st.caption('Train set 70%, Test set 30%; Sampling without replacement')
     
     # Prepare data for regression
     X = X_raw_encoded
     y = y_raw
 
     # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
     # Create and fit the Random Forest regressor
     rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
